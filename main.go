39417ff7676daf336b367041ec37e5d864f365d8
Corto Dufour <cortodufour@gmail.com>
2020-09-17 13:01:11 +0200

    Database & Docker setup

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -17,12 +18,29 @@ func HandleRequest(){
 		fmt.Println("Saucisse ?")
 	})
 
+	router.GET("/", func(c *gin.Context){
+		fmt.Println("WoW")
+	})
+
 	// Listen and serve on 0.0.0.0:8080
 	log.Fatal(router.Run(":8080"))
 }
 
+func dbSetup(){
+	db, err := sql.Open("mysql",
+		"gobdd:gobdd@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
+
 func main() {
 	HandleRequest()
+
+	dbSetup()
+
+	//insert, err := db.Query("J'aodre la sacuisse (1, 'SooS')")
 }
 
 
